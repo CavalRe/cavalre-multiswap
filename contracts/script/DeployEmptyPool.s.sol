ff5ab9ba81cc3f2354ceb35745743439aa1c83f8
Eric Forgy <Eric.Forgy@cavalre.com>
2023-08-16 13:31:57 -0700

    Deployed version

---
+++ contracts/script/DeployEmptyPool.s.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.0;
+
+import "@cavalre/Pool.sol";
+import "forge-std/Script.sol";
+import "forge-std/Test.sol";
+
+contract DeployEmptyPoolScript is Script, Test {
+
+    Pool private pool;
+    uint256 private tau = 1e16;
+
+    function run() external {
+        uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
+        address multisigAddress = vm.envAddress("MULTISIG_ADDRESS");
+         
+        vm.startBroadcast(deployerPrivateKey);
+        pool = new Pool("Multiswap", unicode"β", tau);
+
+        emit log_named_address("pool address", address(pool));
+        emit log_named_address("pool owner", address(pool.owner()));
+        emit log_named_address("sender", msg.sender);
+
+        pool.transferOwnership(multisigAddress);
+    }
+}
