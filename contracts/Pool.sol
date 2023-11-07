36980c807be69d11a4bed6a30f7c821022503da0
Eric Forgy <Eric.Forgy@cavalre.com>
2023-11-06 22:37:01 -0800

    Remove multicall

--- contracts/Pool.sol
+++ contracts/Pool.sol
@@ -413,29 +413,14 @@ contract Pool is IPool, LPToken, ReentrancyGuard {
             address payToken = payTokens[i];
             uint256 amount = amounts[i];
             if (payToken == address(this)) {
-                uint256 contractBalance = balanceOf(address(this));
-                if (contractBalance == 0) {
-                    _burn(sender, amount);
-                } else if (contractBalance < amount) {
-                    _burn(sender, amount - contractBalance);
-                    _burn(address(this), contractBalance);
-                } else {
-                    _burn(address(this), amount);
-                }
+                _burn(sender, amount);
             } else {
-                uint256 externalBalance = IERC20(payToken).balanceOf(
-                    address(this)
+                SafeERC20.safeTransferFrom(
+                    IERC20(payToken),
+                    sender,
+                    address(this),
+                    amount
                 );
-                uint256 internalBalance = _assetState[payToken].balance /
-                    _assetState[payToken].conversion; // Convert from canonical
-                if (externalBalance < internalBalance + amount) {
-                    SafeERC20.safeTransferFrom(
-                        IERC20(payToken),
-                        sender,
-                        address(this),
-                        internalBalance + amount - externalBalance
-                    );
-                }
                 amount *= _assetState[payToken].conversion; // Convert to canonical
                 _updateAssetBalance(payToken, amount, 0);
             }
